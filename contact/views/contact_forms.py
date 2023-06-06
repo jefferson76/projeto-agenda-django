fbf163e5ca3b9ca5f4654a04f9e0061b876a31b8
Jefferson Pereira <jefferson.pereira76@gmail.com>
2023-06-06 14:47:39 -0300

    Criando a view, url e template para delete (apagar contatos)

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -1,4 +1,3 @@
-from django.contrib.auth.decorators import login_required
 from django.shortcuts import get_object_or_404, redirect, render
 from django.urls import reverse
 
@@ -6,22 +5,18 @@ from contact.forms import ContactForm
 from contact.models import Contact
 
 
-@login_required(login_url='contact:login')
 def create(request):
     form_action = reverse('contact:create')
 
     if request.method == 'POST':
-        form = ContactForm(request.POST, request.FILES)
-
+        form = ContactForm(request.POST)
         context = {
             'form': form,
             'form_action': form_action,
         }
 
         if form.is_valid():
-            contact = form.save(commit=False)
-            contact.owner = request.user
-            contact.save()
+            contact = form.save()
             return redirect('contact:update', contact_id=contact.pk)
 
         return render(
@@ -42,16 +37,14 @@ def create(request):
     )
 
 
-@login_required(login_url='contact:login')
 def update(request, contact_id):
     contact = get_object_or_404(
-        Contact, pk=contact_id, show=True, owner=request.user
+        Contact, pk=contact_id, show=True
     )
     form_action = reverse('contact:update', args=(contact_id,))
 
     if request.method == 'POST':
-        form = ContactForm(request.POST, request.FILES, instance=contact)
-
+        form = ContactForm(request.POST, instance=contact)
         context = {
             'form': form,
             'form_action': form_action,
@@ -79,10 +72,9 @@ def update(request, contact_id):
     )
 
 
-@login_required(login_url='contact:login')
 def delete(request, contact_id):
     contact = get_object_or_404(
-        Contact, pk=contact_id, show=True, owner=request.user
+        Contact, pk=contact_id, show=True
     )
     confirmation = request.POST.get('confirmation', 'no')
 
