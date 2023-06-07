678bfd926c7308db7bafa910ac4814fb34172927
Jefferson Pereira <jefferson.pereira76@gmail.com>
2023-06-07 10:11:30 -0300

    Criando a relação entre contact e owner dos contatos

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -16,9 +16,14 @@ def create(request):
             'form': form,
             'form_action': form_action,
         }
+
         if form.is_valid():
             contact = form.save()
+            contact = form.save(commit=False)
+            contact.owner = request.user
+            contact.save()
             return redirect('contact:update', contact_id=contact.pk)
+
         return render(
             request,
             'contact/create.html',
@@ -38,9 +43,10 @@ def create(request):
 @login_required(login_url='contact:login')
 def update(request, contact_id):
     contact = get_object_or_404(
-        Contact, pk=contact_id, show=True
+        Contact, pk=contact_id, show=True, owner=request.user
     )
     form_action = reverse('contact:update', args=(contact_id,))
+
     if request.method == 'POST':
         form = ContactForm(request.POST, request.FILES, instance=contact)
         context = {
@@ -69,9 +75,10 @@ def update(request, contact_id):
 @login_required(login_url='contact:login')
 def delete(request, contact_id):
     contact = get_object_or_404(
-        Contact, pk=contact_id, show=True
+        Contact, pk=contact_id, show=True, owner=request.user
     )
     confirmation = request.POST.get('confirmation', 'no')
+
     if confirmation == 'yes':
         contact.delete()
         return redirect('contact:index')
